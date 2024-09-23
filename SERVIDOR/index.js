08d951a729d6b9ebe0bb668a4d70ddfe91d8a3a4
SEBAS0709 <sebastianhidalgojz@gmail.com>
2024-09-23 09:09:27 -0400

    first commit

--- SERVIDOR/index.js
+++ SERVIDOR/index.js
@@ -1,1 +1,12 @@
-console.log("hello word");
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  console.log('Hello World!!!!!!!!')
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
